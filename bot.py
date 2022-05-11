0f2e49967fb63006394fd73b776b2b35b8c32c7c
Shabalin13 <d.shabalin@innopolis.university>
2022-05-11 22:23:42 +0300

    add data preprocessing

--- bot.py
+++ bot.py
@@ -1,12 +1,47 @@
 import re
 import random
+import json
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+ignore_words = ['?', '!']
 
 
 class ChatBot:
+
     def __init__(self):
         self.model = None
         self.intents_json = None
 
+        self.lemmatizer = WordNetLemmatizer()
+
+        self.words = []
+        self.classes = []
+        self.documents = []
+        data_file = open("intents.json").read()
+        self.intents = json.loads(data_file)
+
+    def data_preprocessing(self):
+        for intent in self.intents['intents']:
+            for pattern in intent['patterns']:
+
+                # tokenize each word
+                w = nltk.word_tokenize(pattern)
+                self.words.extend(w)
+
+                # add documents in the corpus
+                self.documents.append((w, intent['tag']))
+
+                # add to our classes list
+                if intent['tag'] not in self.classes:
+                    self.classes.append(intent['tag'])
+
+        # lemmatize, lower each word and remove duplicates
+        self.words = [self.lemmatizer.lemmatize(w.lower()) for w in self.words if w not in ignore_words]
+        self.words = sorted(list(set(self.words)))
+
+        # sort classes
+        self.classes = sorted(list(set(self.classes)))
 
     def predict_class(self, text, model):
         pass
