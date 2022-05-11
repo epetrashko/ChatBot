50fa4c3470e993cda6f07038fa201f8ef6f66f24
EvgenyPetrashko <e.petrashko@innopolis.university>
2022-05-11 19:25:50 +0300

    intents.json added + local setting for main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import logging
 import os
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from nltk.chat.util import Chat, reflections
+from pairs import pairs
 
 PORT = int(os.environ.get('PORT', 443))
 TOKEN = "5324171051:AAGdPccBNvGEtPDTPOnaBRtiMM5eiNeK2vg"
@@ -10,13 +12,15 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
+chat = Chat(pairs, reflections)
+
 
 def start(update, _):
     update.message.reply_text('Hi! Write some message')
 
 
 def echo(update, _):
-    update.message.reply_text(update.message.text)
+    update.message.reply_text(chat.respond(update.message.text))
 
 
 def error(update, context):
@@ -34,13 +38,13 @@ def main():
     dp.add_handler(CommandHandler("start", start))
 
     # Use it when you want to deploy bot on heroku
-    updater.start_webhook(listen="0.0.0.0",
-                          port=int(PORT),
-                          url_path=TOKEN,
-                          webhook_url='https://brawler-chat-bot.herokuapp.com/' + TOKEN)
+    # updater.start_webhook(listen="0.0.0.0",
+    #                       port=int(PORT),
+    #                       url_path=TOKEN,
+    #                       webhook_url='https://brawler-chat-bot.herokuapp.com/' + TOKEN)
 
     # Use it when you want to test on local machine
-    # updater.start_polling()
+    updater.start_polling()
 
     updater.idle()
 
